76b3529f54f7a1ffb340ae4f17b036cb0a2023f6
phrz <phrz@me.com>
2017-10-26 08:45:17 -0500

    Base project iOS

---
+++ iOS/Choice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Choice
+//
+//  Created by Paul Herz on 2017-10-26.
+//  Copyright © 2017 Paul Herz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
